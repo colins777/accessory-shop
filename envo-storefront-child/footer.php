f51f1000d46ae20399f313cd8ad86d8651a79806
colins777 <rumyga@gmail.com>
2020-05-03 20:36:02 +0300

    3/05/2020 - add payment systems&contacts in footer/fixed input number in product and cart page

--- envo-storefront-child/footer.php
+++ envo-storefront-child/footer.php
@@ -2,13 +2,57 @@
 </div><!-- end page-area -->
 
 <footer class="footer container-fluid">
-	<div class="container">
+    <div class="container">
         <div class="footer-descr">
-            <h3>accessory-shop.com.ua @2020</h3>
-        </div>
-	</div>
+            <div class="footer-column">
+                <div class="footer-contacts">
+                    <div class="header-contact"><a href="tel:+380992212234"><i class="fa fa-phone-square"></i>+38
+                            099-22-12-234</a></div>
+                    <div class="header-contact"><a href="tel:+380992212234"><i class="fa fa-phone-square"></i>+38
+                            099-22-12-234</a></div>
+                    <div class="mail-social-wrap">
+                        <div class="header-contact"><a href="mail:accsessory.office@gmail.com"><i
+                                        class="fa fa-envelope"></i>accsessory.office@gmail.com</a></div>
+
+                    </div>
+                </div>
+
+            </div><!--footer-column-->
+            <div class="footer-column">
+                <h3>accessory-shop.com.ua @2020</h3>
+            </div> <!--footer-column-->
+
+            <div class="footer-column footer-column--right">
+                <span class="icons-title">Работаем с системами платежей Visa и Mastercard</span>
+                <a class="privacy-policy" href="<?php echo get_home_url() .'/privacy-policy';?>">Политика конфеденциальности</a>
+                <div class="icons">
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/icons/payment-systems/icons8-mastercard-logo.svg' ?>" alt="Работа с платежной системой Mastercard"/>
+                    <img src="<?php echo get_stylesheet_directory_uri() . '/img/icons/payment-systems/icons8-visa.svg' ?>" alt="Работа с платежной системой Mastercard"/>
+                </div>
+            </div> <!--footer-column-->
+        </div> <!--footer-descr-->
+
+
+    </div>
+    </div>
 </footer>
 </div><!-- end page-wrap -->
 <?php wp_footer(); ?>
 </body>
 </html>
+
+
+<!--<div class="header-contacts">
+    <div class="header-contact"><a href="tel:+380992212234"><i class="fa fa-phone-square"></i>+38 099-22-12-234</a></div>
+    <div class="mail-social-wrap">
+        <div class="header-contact"><a href="mail:accsessory.office@gmail.com"><i class="fa fa-phone-square"></i>accsessory.office@gmail.com</a></div>
+        <div class="header-contact">
+            <a href="#"><i class="fa fa-instagram"></i></a>
+        </div>
+        <div class="header-contact">
+            <a href="#"><i class="fa fa-telegram"></i></a>
+        </div>
+        <div class="header-contact"><a href="#"><img src="http://accessory-shop.loc/wp-content/uploads/2020/04/viber.png" alt="viber" /></a></div>
+    </div>
+</div>-->
+<!-- header-contacts -->
